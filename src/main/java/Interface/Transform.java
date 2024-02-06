bf9b65151cf151010d90880c7f3f75f4af357e27
YanniSihocine <113210309+YanniSihocine@users.noreply.github.com>
2024-02-05 23:56:18 -0500

    Update Transform.java

--- src/main/java/Interface/Transform.java
+++ src/main/java/Interface/Transform.java
@@ -5,24 +5,16 @@ import Point.Point2d;
 import java.util.Collection;
 
 public class Transform implements Rotate, Translate{
-    /** TODO
-     * Rotate all points of collections given in parameters
-     * with the angle given
-     * @param coords collections of Point2d
-     * @param angle
-     * @return rotated collection
-     * */
+    @Override
     public Collection<Point2d> rotate(Collection<Point2d> coords, Double angle) {
-        return null;
+        coords.forEach(point -> point.rotate(angle));
+        return coords;
     }
 
-    /** TODO
-     * Translate all points of a collection by a given vector
-     * @param coords Collection of Point2d
-     * @param translateVector Vector of translation
-     * @return translated coords
-     * */
+    @Override
     public Collection<Point2d> translate(Collection<Point2d> coords, Point2d translateVector) {
-        return null;
+        coords.forEach(point -> point.translate(translateVector));
+        return coords;
     }
 }
+
