74bea8ae4b74e8c9127e5a4dd2d71a765c7d5fce
MedjdoubiRami <113062178+MedjdoubiRami@users.noreply.github.com>
2024-02-05 22:55:02 -0500

    Update Point2d.java

--- src/main/java/Point/Point2d.java
+++ src/main/java/Point/Point2d.java
@@ -4,106 +4,73 @@ public class Point2d extends AbstractPoint {
     private final Integer X = 0;
     private final Integer Y = 1;
 
-    /** TODO
-     * 2D Point Constructor from coordinates
-     * @param x X coordinate
-     * @param y Y coordinate
-     */
     public Point2d(Double x, Double y) {
-        super(new double[] {0.0, 0.0});
+        super(new double[]{x, y});
     }
 
-    /** TODO
-     * 2D Point Constructor from vector
-     * @param vector Vector containing X and Y coordinates
-     */
     public Point2d(Double[] vector) {
-        this(0.0, 0.0);
+        super(new double[]{vector[0], vector[1]});
     }
 
-    /**
-     * @return X coordinate
-     */
     public double X() { return vector[X];}
 
-    /**
-     * @return Y coordinate
-     */
     public double Y() { return vector[Y];}
 
-    /** TODO
-     * Translate the point by the given vector
-     * @param translateVector The vector by which to translate
-     * @return Translated point
-     */
     @Override
     public Point2d translate(Double[] translateVector) {
-        return null;
+        vector[X] += translateVector[0];
+        vector[Y] += translateVector[1];
+        return this;
     }
 
-    /** TODO
-     * Translate the point by the given 2D Point
-     * @param translateVector The 2D Point by which to translate
-     * @return Translated point
-     */
     public Point2d translate(Point2d translateVector) {
-        return null;
+        vector[X] += translateVector.X();
+        vector[Y] += translateVector.Y();
+        return this;
     }
 
-    /** TODO
-     * Rotate the point by the given rotation Matrix
-     * @param rotationMatrix Matrix by which to rotate
-     * @return Rotated point
-     */
     @Override
     public Point2d rotate(Double[][] rotationMatrix) {
-        return null;
+        double rotatedX = (rotationMatrix[0][0] * vector[X]) + (rotationMatrix[0][1] * vector[Y]);
+        double rotatedY = (rotationMatrix[1][0] * vector[X]) + (rotationMatrix[1][1] * vector[Y]);
+        vector[X] = rotatedX;
+        vector[Y] = rotatedY;
+        return this;
     }
 
-    /** TODO
-     * Rotate the point by the given angle
-     * @param angle Angle in radians
-     * @return Rotated point
-     */
     public Point2d rotate(Double angle) {
-        return null;
+        double sinusAngle = Math.sin(angle);
+        double cosinusAngle = Math.cos(angle);
+        double rotatedX = (cosinusAngle * vector[X]) - (sinusAngle * vector[Y]);
+        double rotatedY = (sinusAngle * vector[X]) + (cosinusAngle * vector[Y]);
+        vector[X] = rotatedX;
+        vector[Y] = rotatedY;
+        return this;
     }
 
-    /** TODO
-     * Divide the X and Y coordinates of a 2D point by a scalar
-     * @param divider Scalar used to divide
-     * @return Divided point
-     */
     @Override
     public Point2d divide(Double divider) {
-        return null;
+        vector[X] /= divider;
+        vector[Y] /= divider;
+        return this;
     }
 
-    /** TODO
-     * Multiply the X and Y coordinates of a 2D point by a scalar
-     * @param multiplier Scalar used to multiply
-     * @return Multiplied point
-     */
     @Override
     public Point2d multiply(Double multiplier) {
-        return null;
+        vector[X] *= multiplier;
+        vector[Y] *= multiplier;
+        return this;
     }
 
-    /** TODO
-     * Add a scalar to the X and Y coordinates of a 2D point
-     * @param adder Scalar to add
-     * @return Added point
-     */
     @Override
     public Point2d add(Double adder) {
-        return null;
+        vector[X] += adder;
+        vector[Y] += adder;
+        return this;
     }
 
-    /** TODO
-     * @return Deep copy of the 2D point
-     */
     @Override
     public Point2d clone() {
-        return null;
+        return new Point2d(vector[X], vector[Y]);
     }
 }
